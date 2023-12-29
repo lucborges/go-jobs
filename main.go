e2ab7089855b6aab412f33d215fc629d16c65de9
lucborges <lucborges@uolinc.com>
2023-12-29 00:19:45 -0300

    feat: insert sqlite orm and configurate a initial database and create a logger

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/lucborges/go-jobs/router"
+import (
+	"github.com/lucborges/go-jobs/config"
+	"github.com/lucborges/go-jobs/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.ErrorF("config initialization error: %v", err)
+		return
+	}
 	router.Initialize()
 }
